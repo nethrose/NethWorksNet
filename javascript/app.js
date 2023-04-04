890772f4ff37e23767a65455b7afd3e571d405fa
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-04 15:57:04 -0700

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,15 @@
 const navLinks = document.querySelectorAll(".nav-links li a");
+const pages = document.querySelectorAll(".page");
 
-navLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-    const section = document.querySelector(href);
-    const topOffset = header.offsetHeight;
-    const elementPosition = section.offsetTop - topOffset;
-
-    window.scrollTo({
-      top: elementPosition,
-      behavior: "smooth"
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const target = link.getAttribute("data-link");
+    pages.forEach((page) => {
+      page.classList.remove("active");
+      if (page.getAttribute("id") === target) {
+        page.classList.add("active");
+      }
     });
-
-    navLinks.forEach(link => link.classList.remove("active"));
-    link.classList.add("active");
-  });
-});
-
-window.addEventListener("scroll", () => {
-  const fromTop = window.scrollY + header.offsetHeight;
-
-  navLinks.forEach(link => {
-    const section = document.querySelector(link.getAttribute("href"));
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-
-    if (fromTop >= sectionTop && fromTop < sectionTop + sectionHeight) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
   });
 });
