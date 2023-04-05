6aca4fc7c31081bf807956f261800a6e0a51387e
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-04-04 18:27:19 -0700

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,5 @@
 const links = document.querySelectorAll("nav a");
-
+const navBar = document.querySelector(".navbar");
 const contentMap = {
   about: `
     <h2>About</h2>
@@ -19,11 +19,59 @@ const contentMap = {
   `,
 };
 
+const titleRect = document.querySelector(".header").getBoundingClientRect();
+
 links.forEach(function (link) {
   link.addEventListener("click", function (event) {
     event.preventDefault();
-    const tab = this.getAttribute("data-tab");
+
+    const activeLink = document.querySelector(".nav-link.active");
+
+    if (activeLink) {
+      // Animate the active link back to its original position
+      activeLink.style.top = "";
+      activeLink.style.left = "";
+      activeLink.style.transform = "";
+    }
+
+    if (activeLink === this) {
+      // If the same link is clicked again, remove the active class
+      this.classList.remove("active");
+      document.getElementById("main-content").innerHTML = "";
+      return;
+    }
+
+    // Remove the active class from the previously active link
+    if (activeLink) {
+      activeLink.classList.remove("active");
+    }
+
+    // Add the active class to the clicked link
+    this.classList.add("active");
+
+    // Move the clicked link to the center under the main title
+    const linkRect = this.getBoundingClientRect();
+    const x = titleRect.left + (titleRect.width / 2);
+    const y = titleRect.bottom + 20;
+    const dx = x - (linkRect.left + (linkRect.width / 2));
+    const dy = y - (linkRect.top + (linkRect.height / 2));
+
+    this.style.top = `${linkRect.top + dy}px`;
+    this.style.left = `${linkRect.left + dx}px`;
+    this.style.transform = "";
+
     const mainContent = document.getElementById("main-content");
-    mainContent.innerHTML = contentMap[tab];
+
+    // Fade out the existing content
+    mainContent.style.opacity = 0;
+
+    // Wait for the animation to complete
+    setTimeout(() => {
+      // Update content
+      mainContent.innerHTML = contentMap[this.getAttribute("data-tab")];
+
+      // Fade in the new content
+      mainContent.style.opacity = 1;
+    }, 1000); // 1s matches the CSS transition duration
   });
 });
