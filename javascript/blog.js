bbab33373e2a012014233b089a77c717b7d22a47
Neth Rose <6612612+nethrose@users.noreply.github.com>
2023-05-24 18:59:30 -0700

    Update blog.js

--- javascript/blog.js
+++ javascript/blog.js
@@ -29,7 +29,6 @@ export async function loadBlogPosts() {
   });
 }
 
-
 export const blogContent = async () => {
   const blogPosts = await loadBlogPosts();
   const blogPostsHTML = `
@@ -44,50 +43,7 @@ export const blogContent = async () => {
   };
 }
 
-function waitForMarked(callback) {
-  if (typeof marked === 'function') {
-    console.log('Marked is available, invoking callback');
-    console.log('Testing marked:', marked('# Hello, world!'));
-    callback();
-  } else {
-    console.log('Marked is not available, retrying in 100ms');
-    setTimeout(() => waitForMarked(callback), 100);
-  }
-}
-
-
-export async function handleBlogPostClick(event) {
-  console.log('Blog post link clicked');
-  event.preventDefault();
-  const postFileName = event.target.getAttribute("data-post");
-  console.log('Clicked post:', postFileName);
-
-  waitForMarked(async () => {
-    try {
-      const response = await fetch(`/blog/${postFileName}`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const content = await response.text();
-      console.log('Fetched content:', content);
-      const htmlContent = marked(content);
-      console.log('HTML content:', htmlContent);
-
-      const mainContent = document.getElementById("main-content");
-      mainContent.style.opacity = 0;
-      mainContent.innerHTML = `<div class="rendered-content">${htmlContent}</div>`;
-
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  });
-}
-
 export function attachBlogPostClickListeners() {
   const blogPostLinks = document.querySelectorAll(".blog-post-link");
-  console.log(`Found ${blogPostLinks.length} blog post links`); 
-  blogPostLinks.forEach((link) => {
-    console.log(`Attaching event listener to ${link}`); 
-    link.addEventListener("click", handleBlogPostClick);
-  });
+  console.log(`Found ${blogPostLinks.length} blog post links`);
 }
